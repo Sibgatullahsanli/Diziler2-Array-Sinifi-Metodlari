15304088d1b2ac26686a3dfd246bd358e8149767
Sıbgatullah <sibgat.sanli@gmail.com>
2022-09-02 01:12:35 +0300

    IndexOf ve Resize metodları eklendi.

--- Program.cs
+++ Program.cs
@@ -27,11 +27,23 @@
         // Reverse Metodu (Verdiğimiz diziyi ortadan aynalıyomuş gbi yer değiştiriyor) 
         Console.WriteLine("***** Array Reverse *****");
         Array.Reverse(sayiDizisi);
-        
+
         foreach (var sayi in sayiDizisi)
           Console.WriteLine(sayi);
 
+        // IndexOf Metodu (Verdiğimiz dizi içerisinde eyer o eleman varsa onun index ini verir bize.)  
+        Console.WriteLine("***** Array IndexOf *****");
+        Array.IndexOf(sayiDizisi,23);
 
-       
+        Console.WriteLine(Array.IndexOf(sayiDizisi,23));
+
+        // Resize Metodu ( Yeniden boyutlandırma yapıyor.)
+        Console.WriteLine("***** Array Resize *****");
+        Array.Resize<int>(ref sayiDizisi,9);         // Burada dedik ki git sayiDizisi nin elemanla sayısını 9 a çıkar.
+        sayiDizisi[8] = 99;                          // Burada da 9 elemana yani 8.ci index teki yeni elemana da 99 sayısını ata dedik.
+
+        foreach (var sayi in sayiDizisi)
+          Console.WriteLine(sayi);
+
     }
 }
