4c4e2c654808b8e169890511f2e004b6bcfc5824
Sıbgatullah <sibgat.sanli@gmail.com>
2022-09-02 00:03:58 +0300

    Sort metodu eklendi

--- Program.cs
+++ Program.cs
@@ -2,6 +2,17 @@
 {
     private static void Main(string[] args)
     {
+       // Sort Metodu(sıralama)
+       int [] sayiDizisi = {23,12,4,86,72,3,11,17};
+       Console.WriteLine("***** Sırasız Dizi *****");
+       foreach (var sayi in sayiDizisi)
+          Console.WriteLine(sayi);
+        
+        Console.WriteLine("***** Sıralı Dizi *****");
+        Array.Sort(sayiDizisi);
+
+        foreach (var sayi in sayiDizisi)
+          Console.WriteLine(sayi);
        
     }
 }
